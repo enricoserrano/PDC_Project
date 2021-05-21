68e9464542eeae8260388e94d79cb19a848bed1b
enricoserrano <69224805+enricoserrano@users.noreply.github.com>
2021-05-21 12:34:54 +1200

    PDC_Porject_v3.0.0
    -Created a derby database connection
    -Added a simple leaderboard database which has manual data entry
    -Created a leaderboard screen
    -Modified leaderboard screen to support the display of leaderboard database

---
+++ SC_ProjectGUI_3.0.0/src/userGUI/LoginVariables.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userGUI;
+
+/**
+ *
+ * @author Enrico
+ */
+public class LoginVariables {
+
+    private static String userName;
+    private static int userScore;
+
+    public static String getUserName() {
+        return userName;
+    }
+
+    public static void setUserName(String userName) {
+        LoginVariables.userName = userName;
+    }
+
+    public static int getUserScore() {
+        return userScore;
+    }
+
+    public static void setUserScore(int userScore) {
+        LoginVariables.userScore = userScore;
+    }
+
+    public LoginVariables() {
+    }
+
+    
+}
